d46029707ac490732441278ad7070eb09dcd4087
pankop <zakymuslim7@gmail.com>
2025-05-22 13:47:21 +0700

    PR pertemuan 11, di lokal saya sudah dibuat banyak route, melainkan di github tidak diupdate pak

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 //
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend');
 });
 
 
@@ -18,4 +18,32 @@ Route::get('blog', function () {
 
 Route::get('satu', function () {
     return view('pertama');
+});
+
+Route::get('boostrap1', function () {
+    return view('boostrap1');
+});
+
+Route::get('ets', function () {
+    return view('ets');
+});
+
+Route::get('js1', function () {
+    return view('js1');
+});
+
+Route::get('linktree', function () {
+    return view('linktree');
+});
+
+Route::get('template', function () {
+    return view('template');
+});
+
+Route::get('validasi1', function () {
+    return view('validasi1');
+});
+
+Route::get('pertemuan4', function () {
+    return view('pertemuan4');
 });
