626335a61280398473307dd7ba0ac50611cac893
pankop <zakymuslim7@gmail.com>
2025-06-05 16:09:27 +0700

    pertemuan 14

---
+++ resources/views/index.blade.php
@@ -0,0 +1,41 @@
+@extends('template')
+
+@section('content')
+    <h3>Data Pegawai</h3>
+
+    <a href="/pegawai/tambah" class="btn btn-info"> + Tambah Pegawai Baru</a>
+
+{{--    form inline ini biar dia dipaksa sebaris--}}
+    <form action="/pegawai/cari" method="GET" class="form-inline">
+        <label class="form-label pr-2">Cari Data Pegawai :</label>
+        <input type="text" name="cari" placeholder="Cari Pegawai .." class="form-control m-1">
+        <input type="submit" value="CARI" class="btn btn-primary">
+    </form>
+    <br />
+
+    <table class="table table-striped">
+        <tr>
+            <th>Nama</th>
+            <th>Jabatan</th>
+            <th>Umur</th>
+            <th>Alamat</th>
+            <th>Opsi</th>
+        </tr>
+        @foreach ($pegawai as $p)
+            <tr>
+                <td>{{ $p->pegawai_nama }}</td>
+                <td>{{ $p->pegawai_jabatan }}</td>
+                <td>{{ $p->pegawai_umur }}</td>
+                <td>{{ $p->pegawai_alamat }}</td>
+                <td>
+                    <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-success">Edit</a>
+                    <a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    {{ $pegawai->links() }} {{--hanya bisa dipakai dengan paginate, saat get harus dihapus--}}
+@endsection
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PegawaiDBController;
 use Illuminate\Support\Facades\Route;
 //
 
@@ -54,7 +55,7 @@ Route::get('pertemuan4', function () {
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('welcome', [DosenController::class, 'welcome']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+//Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 
@@ -63,3 +64,13 @@ Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 Route::get('/blog',[BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+//route pegawaiDB
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']); //jika form dikirim, route ini akan dijalankan
+Route::get('/pegawai/edit/{id}',[PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update',[PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
